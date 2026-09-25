4ce2d55c77f0d1d808f6fa25eb3c8a4e3af01b82
luisToti1931 <luissanchezz1931@gmail.com>
2026-09-25 15:46:33 +0200

    cambios en el ej2

--- 2/Acceso_a_datos/Primer_Trimestre/Relacion1/PrepararCarpetas.java
+++ 2/Acceso_a_datos/Primer_Trimestre/Relacion1/PrepararCarpetas.java
@@ -6,7 +6,7 @@ import java.nio.file.Path;
 public class PrepararCarpetas {
     public static void main(String[] args) {
         Path carpeta1 = Path.of("datos");
-        Path carpeta2 = Path.of("datos/copias");
+        Path carpeta2 = carpeta1.resolve("copias");
 
         Path archivo1 = carpeta1.resolve("clubes.txt");
         Path archivo2 = carpeta2.resolve("respaldo.txt");
